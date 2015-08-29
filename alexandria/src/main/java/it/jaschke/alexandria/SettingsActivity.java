5ab58813a464ed8f92c9ec567602f6d61d88f7e3
Jan Grünewald <jan.gruenewald84@googlemail.com>
2015-08-30 00:53:26 +0200

    update: added AppCompatDelegate to support an actionbar in settingsactivity

--- alexandria/src/main/java/it/jaschke/alexandria/SettingsActivity.java
+++ alexandria/src/main/java/it/jaschke/alexandria/SettingsActivity.java
@@ -1,18 +1,113 @@
 package it.jaschke.alexandria;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.support.annotation.LayoutRes;
+import android.support.v7.app.AppCompatDelegate;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by saj on 27/01/15.
+ *
+ * Modified by JG on 30/08/15:
+ * Inspired by "AppCompatPreferenceActivity" by Google:
+ * https://chromium.googlesource.com/android_tools/+/7200281446186c7192cb02f54dc2b38e02d705e5/sdk/extras/android/support/samples/Support7Demos/src/com/example/android/supportv7/app/AppCompatPreferenceActivity.java
  */
 public class SettingsActivity extends PreferenceActivity {
 
+    private AppCompatDelegate mDelegate;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        getDelegate().installViewFactory();
+        getDelegate().onCreate(savedInstanceState);
+
         super.onCreate(savedInstanceState);
+
+        // this is deprecated since API 11; pre 11 there are no PreferenceFragments so this
+        // method will be used anyways
+        //noinspection deprecation
         addPreferencesFromResource(R.xml.preferences);
 
+    }
 
+    @Override public boolean onOptionsItemSelected(MenuItem item) {
+        // leave the activity when home-button is pressed
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        getDelegate().onPostCreate(savedInstanceState);
+    }
+
+    @Override
+    public void setContentView(@LayoutRes int layoutResID) {
+        getDelegate().setContentView(layoutResID);
+    }
+
+    @Override
+    public void setContentView(View view) {
+        getDelegate().setContentView(view);
+    }
+
+    @Override
+    public void setContentView(View view, ViewGroup.LayoutParams params) {
+        getDelegate().setContentView(view, params);
+    }
+
+    @Override
+    public void addContentView(View view, ViewGroup.LayoutParams params) {
+        getDelegate().addContentView(view, params);
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        getDelegate().onPostResume();
+    }
+
+    @Override
+    protected void onTitleChanged(CharSequence title, int color) {
+        super.onTitleChanged(title, color);
+        getDelegate().setTitle(title);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        getDelegate().onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        getDelegate().onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        getDelegate().onDestroy();
+    }
+
+    public void invalidateOptionsMenu() {
+        getDelegate().invalidateOptionsMenu();
+    }
+
+    private AppCompatDelegate getDelegate() {
+        if (mDelegate == null) {
+            mDelegate = AppCompatDelegate.create(this, null);
+        }
+        return mDelegate;
     }
 }
