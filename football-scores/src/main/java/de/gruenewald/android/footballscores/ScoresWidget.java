08e3a19274e3427667976d8ccc1579a74418afd4
Jan Grünewald <jan.gruenewald84@googlemail.com>
2015-10-10 11:47:45 +0200

    added: initially added a scores_widget with keyguard support

---
+++ football-scores/src/main/java/de/gruenewald/android/footballscores/ScoresWidget.java
@@ -0,0 +1,72 @@
+/*
+ * *
+ *  * ****************************************************************************
+ *  * Copyright (c) 2015 by Jan Grünewald.
+ *  * jan.gruenewald84@googlemail.com
+ *  * <p>
+ *  * This file is part of 'Super Duo'. 'Super Duo' was developed as
+ *  * part of the Android Developer Nanodegree by Udacity.
+ *  * <p>
+ *  * 'Super Duo' is free software: you can redistribute it and/or modify
+ *  * it under the terms of the GNU General Public License as published by
+ *  * the Free Software Foundation, either version 3 of the License, or
+ *  * (at your option) any later version.
+ *  * <p>
+ *  * 'Super Duo' is distributed in the hope that it will be useful,
+ *  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  * GNU General Public License for more details.
+ *  * <p>
+ *  * You should have received a copy of the GNU General Public License
+ *  * along with 'Super Duo'.  If not, see <http://www.gnu.org/licenses/>.
+ *  * ****************************************************************************
+ *
+ */
+
+package de.gruenewald.android.footballscores;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.widget.RemoteViews;
+
+import barqsoft.footballscores.R;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class ScoresWidget extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        final int N = appWidgetIds.length;
+        for (int i = 0; i < N; i++) {
+            updateAppWidget(context, appWidgetManager, appWidgetIds[i]);
+        }
+    }
+
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.scores_widget);
+        views.setTextViewText(R.id.appwidget_text, widgetText);
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+}
+
