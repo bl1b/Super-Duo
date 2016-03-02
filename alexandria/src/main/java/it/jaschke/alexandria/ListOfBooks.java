f93f9754dbae02eba5ab48e8bff34460e4bbbda5
Jan Grünewald <gruenewald@cominto.de>
2016-03-02 18:08:29 +0100

    removed unused imports

--- alexandria/src/main/java/it/jaschke/alexandria/ListOfBooks.java
+++ alexandria/src/main/java/it/jaschke/alexandria/ListOfBooks.java
@@ -1,13 +1,11 @@
 package it.jaschke.alexandria;
 
-import android.app.Activity;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
